a8219d4d13de7ee134c1d4564c25b48528c05c8c
HUY DINH <dinhhuy574@gmail.com>
2025-01-09 09:00:56 +0700

    List of product review

---
+++ client/src/pages/ProductDetails/ProductReview/reviews_list.js
@@ -0,0 +1,40 @@
+
+const reviews_list = [
+    {
+        username: 'ABCCCCCCCCCCCCCCCCCCCCCCCc',
+        profile_image: 'https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=612x612&w=0&k=20&c=tyLvtzutRh22j9GqSGI33Z4HpIwv9vL_MZw_xOE19NQ=',
+        rating: 3,
+        review: "This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good This product looks good ",
+
+    },
+    {
+        username: 'ABC',
+        profile_image: 'https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=612x612&w=0&k=20&c=tyLvtzutRh22j9GqSGI33Z4HpIwv9vL_MZw_xOE19NQ=',
+        rating: 3,
+        review: "This product looks good",
+
+    },
+    {
+        username: 'ABC',
+        profile_image: 'https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=612x612&w=0&k=20&c=tyLvtzutRh22j9GqSGI33Z4HpIwv9vL_MZw_xOE19NQ=',
+        rating: 3,
+        review: "This product looks good",
+
+    },
+    {
+        username: 'ABC',
+        profile_image: 'https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=612x612&w=0&k=20&c=tyLvtzutRh22j9GqSGI33Z4HpIwv9vL_MZw_xOE19NQ=',
+        rating: 3,
+        review: "This product looks good",
+
+    },
+    {
+        username: 'ABC',
+        profile_image: 'https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=612x612&w=0&k=20&c=tyLvtzutRh22j9GqSGI33Z4HpIwv9vL_MZw_xOE19NQ=',
+        rating: 3,
+        review: "This product looks good",
+
+    },
+]
+
+export default reviews_list;
