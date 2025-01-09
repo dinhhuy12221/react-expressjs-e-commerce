7102684aa7f0edd429085d20de24f4936909bbbb
HUY DINH <dinhhuy574@gmail.com>
2025-01-09 22:23:20 +0700

    Add loop when swiping products

--- client/src/pages/ProductDetails/RelatedProducts/index.js
+++ client/src/pages/ProductDetails/RelatedProducts/index.js
@@ -1,59 +1,60 @@
-import React from 'react'
+import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Navigation } from "swiper/modules";
 
-import ProductItem from '../../../components/ProductItem';
+import ProductItem from "../../../components/ProductItem";
 export default function RelatedProducts(props) {
   return (
     <>
-      <div className="product-row w-100 mt-4">
-      <div className="d-flex align-items-center">
-                <div className="info w-75">
-                  <h5 className="mb-2">{props.title}</h5>
-                </div>
-              </div>
-                <Swiper
-                  slidesPerView={4}
-                  spaceBetween={0}
-                  navigation={true}
-                  modules={[Navigation]}
-                  pagination={{
-                    clickable: true,
-                  }}
-                  className="mySwiper"
-                >
-                  <SwiperSlide>
-                    <ProductItem></ProductItem>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <ProductItem></ProductItem>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <ProductItem></ProductItem>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <ProductItem></ProductItem>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <ProductItem></ProductItem>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <ProductItem></ProductItem>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <ProductItem></ProductItem>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <ProductItem></ProductItem>
-                  </SwiperSlide>
-                  <SwiperSlide>
-                    <ProductItem></ProductItem>
-                  </SwiperSlide>
-                </Swiper>
-              </div>
+      <div className="product-row mt-4">
+        <div className="d-flex align-items-center">
+          <div className="info w-75">
+            <h5 className="mb-2">{props.title}</h5>
+          </div>
+        </div>
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={0}
+          navigation={true}
+          loop
+          modules={[Navigation]}
+          pagination={{
+            clickable: true,
+          }}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <ProductItem></ProductItem>
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductItem></ProductItem>
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductItem></ProductItem>
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductItem></ProductItem>
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductItem></ProductItem>
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductItem></ProductItem>
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductItem></ProductItem>
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductItem></ProductItem>
+          </SwiperSlide>
+          <SwiperSlide>
+            <ProductItem></ProductItem>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </>
-  )
+  );
 }
