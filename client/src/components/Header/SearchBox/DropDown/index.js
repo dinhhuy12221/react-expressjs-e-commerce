b1526e6464c2a77955b41f01de90122daf972f07
HUY DINH <dinhhuy574@gmail.com>
2025-01-11 11:12:30 +0700

    Create drop down for search result

---
+++ client/src/components/Header/SearchBox/DropDown/index.js
@@ -0,0 +1,24 @@
+import { memo } from "react";
+import ResultItem from "../ResultItem";
+import LoadingAnimation from "../../../LoadingAnimation";
+import "./index.css";
+
+const list = Array(10).fill({
+  title: "Angie’s Boomchickapop Sweet & Salty Kettle Corn",
+  image:
+    "https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-60.jpg",
+});
+
+function DropDown({ ref }) {
+  return (
+    <div ref={ref} className="search-result">
+      <LoadingAnimation>
+        {list.map((item, index) => (
+          <ResultItem key={index} item={item}></ResultItem>
+        ))}
+      </LoadingAnimation>
+    </div>
+  );
+}
+
+export default memo(DropDown);
