bc522df84a4bded7c393258307fa32aa06706712
HUY DINH <dinhhuy574@gmail.com>
2025-01-13 09:41:37 +0700

    Modify config db

--- server/config/db/index.js
+++ server/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 async function connect () {
     try {
         await mongoose.connect(process.env.CONNECTION_STRING);
-        console.log("Connected sucessfully");
+        console.log("Connected db sucessfully");
     } catch (error) {
         console.log(error);
     }
