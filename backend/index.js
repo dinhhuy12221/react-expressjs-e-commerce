4512436a93abc300fc7a7c7a7ff84a860ce73cb3
HUY DINH <dinhhuy574@gmail.com>
2025-01-19 10:05:53 +0700

    Change and add cors

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import bodyParser from "body-parser"
 // import morgan from "morgan"
 import cors from "cors"
 import route from './routes/index.js'
@@ -8,29 +7,32 @@ import path from "path"
 
 import "dotenv/config";
 
+import { connect } from "./config/db/index.js";
 const app = express();
+
 app.use(cors());
 app.options("/", cors());
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
 
 // Database
-import { connect } from "./config/db/index.js";
-connect();
 
 // //Logger
 // var accessLogStream = fs.createWriteStream(
-//   path.join(__dirname, './log/access.log'), 
-//   {
-//     flags: 'a',
-//   }
-// );
-
-// app.use(morgan('combined', { stream: accessLogStream }));
-
-route(app)
-
-// server
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running http://localhost:${process.env.PORT}`);
-});
+  //   path.join(__dirname, './log/access.log'), 
+  //   {
+    //     flags: 'a',
+    //   }
+    // );
+    
+    // app.use(morgan('combined', { stream: accessLogStream }));
+    
+    route(app)
+    
+    // server
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running http://localhost:${process.env.PORT}`);
+      connect();
+    });
+    
