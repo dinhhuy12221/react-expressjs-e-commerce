1bb0babe06ebf3ae61a5644726565532d137f8a7
HUY DINH <dinhhuy574@gmail.com>
2025-01-19 10:04:01 +0700

    Add model user

---
+++ backend/models/user.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+import mongooseDelete from "mongoose-delete";
+import Inc from "mongoose-sequence";
+
+const AutoIncrement = Inc(mongoose);
+
+const Schema = mongoose.Schema;
+
+const userSchema = Schema(
+  {
+    _id: {
+      type: Number,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    _id: false,
+    timestamp: true,
+  }
+);
+
+
+userSchema.plugin(AutoIncrement, { id: "user_id_counter" });
+
+userSchema.plugin(mongooseDelete, {
+  deletedAt: true,
+  overrideMethods: "all",
+});
+
+export default mongoose.model("User", userSchema);
