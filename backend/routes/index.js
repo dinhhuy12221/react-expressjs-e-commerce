63a9fb746b9f52f45c10e35e3cc2cdd1a09086ea
HUY DINH <dinhhuy574@gmail.com>
2025-01-19 10:42:30 +0700

    Modify authRoute

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 import categoryRouter from './categoryRoutes.js'
 import productRouter from './productRoutes.js'
+import authRoutes from './authRoutes.js';
 
 const route = (app) => {
     app.use('/api/category', categoryRouter);
     app.use('/api/product', productRouter);
+    app.use('/api/auth', authRoutes);
 }
 
 export default route;
