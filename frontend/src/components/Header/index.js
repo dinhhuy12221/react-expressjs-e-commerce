989f2d8e05014a71fd59d4d49bb5ea4c102cf217
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2025-12-22 11:24:12 +0700

    Update css

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -9,7 +9,7 @@ import { MyContext } from "../../App";
 import { FaRegUserCircle } from "react-icons/fa";
 import { TiShoppingCart } from "react-icons/ti";
 
-import './index.css'
+import "./index.css";
 import { useSelector } from "react-redux";
 import { selectCurrentCustomer } from "../../features/auth/authSlice";
 import Profile from "./Profile";
@@ -19,7 +19,6 @@ function Header() {
   const customer = useSelector(selectCurrentCustomer);
 
   console.log(customer);
-  
 
   return (
     <>
@@ -34,44 +33,38 @@ function Header() {
         </div>
 
         <header className="header">
-          <div className="container-fluid">
-            <div className="row">
-              <div className="logoWrapper">
-                <Link to={"/"}>
-                  <img src={Logo} alt="Logo"></img>
+          <div className="logoWrapper">
+            <Link to={"/"}>
+              <img src={Logo} alt="Logo"></img>
+            </Link>
+          </div>
+
+          <div className="d-flex align-items-center part2">
+            {context.countryList.length !== 0 && <CountryDropDown />}
+            {/* Header Search start here  */}
+            <SearchBox />
+            {/* Header Search ends here  */}
+
+            <div className="part3 d-flex align-items-center ml-auto">
+              {!customer ? (
+                <Link to="/login">
+                  <Button className="btn-red btn-round signin">Sign In</Button>
                 </Link>
-              </div>
+              ) : (
+                <Profile customer={customer} />
+              )}
 
-              <div className="d-flex align-items-center part2">
-                {context.countryList.length !== 0 && <CountryDropDown />}
-                {/* Header Search start here  */}
-                <SearchBox />
-                {/* Header Search ends here  */}
-
-                <div className="part3 d-flex align-items-center ml-auto">
-                  { !customer ? (
-                    <Link to="/login">
-                      <Button className="btn-red btn-round signin">
-                        Sign In
-                      </Button>
-                    </Link>
-                  ) : (
-                    <Profile customer={customer}/>
-                  )}
-
-                  <div className="ms-auto cartTab d-flex align-items-center">
-                    <span className="price">$55.5</span>
-                    <div className="position-relative me-2">
-                      <Link to={'/cart'}>
-                        <Button className="circle ms-3">
-                          <TiShoppingCart />
-                        </Button>
-                      </Link>
-                      <span className="count d-flex align-items-center justify-content-center">
-                        1
-                      </span>
-                    </div>
-                  </div>
+              <div className="ms-auto cartTab d-flex align-items-center">
+                <span className="price">$55.5</span>
+                <div className="position-relative me-2">
+                  <Link to={"/cart"}>
+                    <Button className="circle ms-3">
+                      <TiShoppingCart />
+                    </Button>
+                  </Link>
+                  <span className="count d-flex align-items-center justify-content-center">
+                    1
+                  </span>
                 </div>
               </div>
             </div>
