216063277211205b040df9eeaf196df3ddeeea66
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2025-12-25 23:01:59 +0700

    Update css

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,12 +1,10 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import Button from "@mui/material/Button";
 import Logo from "../../assets/images/logo.png";
 import CountryDropDown from "../CountryDropdown/index";
 import SearchBox from "./SearchBox/index";
 import Navigation from "./Navigation/index";
 import { MyContext } from "../../App";
-import { FaRegUserCircle } from "react-icons/fa";
 import { TiShoppingCart } from "react-icons/ti";
 import { useSelector } from "react-redux";
 import { selectCurrentCustomer } from "../../features/auth/authSlice";
@@ -43,21 +41,18 @@ function Header() {
               <SearchBox />
               {/* Header Search ends here  */}
                 {!customer ? (
-                  <Link to="/login">
-                    <button className="btn btn--primary signin">Sign In</button>
+                  <Link to="/login" className="btn btn--primary btn--signin">Sign In
                   </Link>
                 ) : (
                   <Profile customer={customer} />
                 )}
   
-                <div className="ms-auto cartTab d-flex align-items-center">
+                <div className="cartTab d-flex align-items-center justify-content-between">
                   <span className="price">$55.5</span>
                   <div className="position-relative me-2">
-                    <Link to={"/cart"}>
-                      <Button className="btn circle ms-3">
+                    <a href="/cart" className="btn btn--circle btn--outlined btn--cart">
                         <TiShoppingCart />
-                      </Button>
-                    </Link>
+                    </a>
                     <span className="count d-flex align-items-center justify-content-center">
                       1
                     </span>
