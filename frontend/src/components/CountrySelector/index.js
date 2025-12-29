74cb73ed919db80d13e038843c806446390f199d
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2025-12-29 11:34:01 +0700

    Update css

--- frontend/src/components/CountrySelector/index.js
+++ frontend/src/components/CountrySelector/index.js
@@ -1,28 +1,27 @@
-import React, { useContext, useEffect } from 'react';
-import { useState } from 'react';
+import React, { useContext, useEffect } from "react";
+import { useState } from "react";
 
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import { FaAngleDown } from "react-icons/fa";
 import { CiSearch } from "react-icons/ci";
 import { IoClose } from "react-icons/io5";
-import DialogTitle from '@mui/material/DialogTitle';
-import Dialog from '@mui/material/Dialog';
-import Slide from '@mui/material/Slide';
-import { MyContext } from '../../App';
+import DialogTitle from "@mui/material/DialogTitle";
+import Dialog from "@mui/material/Dialog";
+import Slide from "@mui/material/Slide";
+import { MyContext } from "../../App";
 
-import './index.css';
+import "./index.css";
 
 const Transition = React.forwardRef(function Transition(
   props: TransitionProps & {
-    children: React.ReactElement<any, any>;
+    children: React.ReactElement<any, any>,
   },
-  ref: React.Ref<unknown>,
+  ref: React.Ref<unknown>
 ) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
 export default function CountrySelector() {
-
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [selectedTab, setSelectedtab] = useState(null);
   const [countryList, setCountryList] = useState([]);
@@ -33,11 +32,11 @@ export default function CountrySelector() {
     setSelectedtab(index);
     setIsOpenModal(false);
     context.setSelectedCountry(country);
-  }
+  };
 
   useEffect(() => {
     setCountryList(context.countryList);
-  }, [])
+  }, []);
 
   const filterList = (e) => {
     const keyword = e.target.value.toLowerCase();
@@ -47,42 +46,67 @@ export default function CountrySelector() {
         return item.country.toLowerCase().includes(keyword);
       });
       setCountryList(list);
-    }
-    else {
+    } else {
       setCountryList(context.countryList);
     }
-  }
+  };
 
   return (
     <>
-      <button className='country-selector' onClick={() => setIsOpenModal(true)}>
-          <div className='country-info'>
-              <span className='country-label'>Your Location</span>
-              <span className='country-name'>{context.selectedCountry !== '' ? (context.selectedCountry.length > 10 ? context.selectedCountry.substring(0, 10) + '...' : context.selectedCountry) : 'Select a location'}</span>
-          </div>
-          <FaAngleDown />
+      <button className="country-selector" onClick={() => setIsOpenModal(true)}>
+        <div className="country-info">
+          <span className="country-label">Your Location</span>
+          <span className="country-name">
+            {context.selectedCountry !== ""
+              ? context.selectedCountry.length > 10
+                ? context.selectedCountry.substring(0, 10) + "..."
+                : context.selectedCountry
+              : "Select a location"}
+          </span>
+        </div>
+        <FaAngleDown />
       </button>
-      <Dialog open={isOpenModal} onClose={() => setIsOpenModal(false)} TransitionComponent={Transition}>
-        <div className='country-modal'>
-          <h5 className='country-modal-label'>Choose your Delivery Country</h5>
-        <p>Enter your address and we will specify the offer for your area.</p>
-        <button className='btn close-button' onClick={() => setIsOpenModal(false)}><IoClose /></button>
-        <div className='country-container'>
-          <div className='country-search'>
-            <CiSearch />
-            <input type='text' placeholder='Search your area...' onChange={filterList}/>
-          </div>
-  
-          <ul className='country-list'>
-            {
-              countryList.length !== 0 && countryList?.map((item, index) => {
-                return (<li key={index}><button onClick={() => selectCountry(index, item.country)} className={`btn ${selectedTab === index ? 'active' : ''}`}>{item.country}</button></li>);
-              })
-            }
-          </ul>
+      <Dialog
+        open={isOpenModal}
+        onClose={() => setIsOpenModal(false)}
+        TransitionComponent={Transition}
+      >
+        <div className="country-modal">
+          <h5 className="country-modal-label">Choose your Delivery Country</h5>
+          <p>Enter your address and we will specify the offer for your area.</p>
+          <button
+            className="btn close-button"
+            onClick={() => setIsOpenModal(false)}
+          >
+            <IoClose />
+          </button>
+          <div className="country-container">
+            <div className="country-search">
+              <CiSearch />
+              <input
+                type="text"
+                placeholder="Search your area..."
+                onChange={filterList}
+              />
+            </div>
+
+            <ul className="country-list">
+              {countryList.length !== 0 &&
+                countryList?.map((item, index) => {
+                  return (
+                    <li
+                      key={index}
+                      onClick={() => selectCountry(index, item.country)}
+                      className={`${selectedTab === index ? "active" : ""}`}
+                    >
+                      {item.country}
+                    </li>
+                  );
+                })}
+            </ul>
           </div>
         </div>
       </Dialog>
-  </>
-  )
+    </>
+  );
 }
