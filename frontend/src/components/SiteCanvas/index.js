b1b8610004345fa4052e4f2ce060a108174612c5
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-01-01 00:44:56 +0700

    Update

--- frontend/src/components/SiteCanvas/index.js
+++ frontend/src/components/SiteCanvas/index.js
@@ -1,14 +1,23 @@
 import { IoClose } from 'react-icons/io5'
+import CountrySelector from '../CountrySelector/index'
 import Logo from '../../assets/images/logo.png'
 import "./index.css"
+import { useContext } from 'react'
+import { MyContext } from '../../App'
 function SiteCanvas() {
+  const context = useContext(MyContext);
+
   return (
     <div className='site-canvas'>
       <div className='canvas-header'>
         <a href='/'><img src={Logo} alt='Logo'></img></a>
-        <button className='btn'><IoClose /></button>
+        <button className='btn close-button'><IoClose /></button>
       </div>
-      <div className='canvas-main'></div>
+      <div className='canvas-main'>
+        {
+          context.countryList.length !== 0 && (<CountrySelector />)
+        }
+      </div>
       <div className='canvas-footer'></div>
     </div>
   )
