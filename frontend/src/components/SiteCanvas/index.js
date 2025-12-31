1f01834d80c15e11b9e68ad837cff463e803cbcd
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-01-01 00:20:51 +0700

    Update

--- frontend/src/components/SiteCanvas/index.js
+++ frontend/src/components/SiteCanvas/index.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import { IoClose } from 'react-icons/io5'
+import Logo from '../../assets/images/logo.png'
 import "./index.css"
 function SiteCanvas() {
   return (
     <div className='site-canvas'>
-      <div className='canvas-header'></div>
+      <div className='canvas-header'>
+        <a href='/'><img src={Logo} alt='Logo'></img></a>
+        <button className='btn'><IoClose /></button>
+      </div>
       <div className='canvas-main'></div>
       <div className='canvas-footer'></div>
     </div>
