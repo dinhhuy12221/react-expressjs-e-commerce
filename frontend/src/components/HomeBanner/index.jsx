f5ae3b62c2609028901a40161d85fa793083dd09
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-01-15 22:13:34 +0700

    Update

--- frontend/src/components/HomeBanner/index.jsx
+++ frontend/src/components/HomeBanner/index.jsx
@@ -9,30 +9,30 @@ import "./index.css";
 
 export default function HomeBanner() {
   return (
-      <div className="home-banner">
-        <div className="blank-div"></div>
-        <Swiper
-          slidesPerView={1}
-          // spaceBetween={20}
-          // navigation={true}
-          pagination={true}
-          loop={true}
-          modules={[Navigation, Autoplay, Pagination]}
-          // autoplay={{
-          //   delay: 2500,
-          //   // disableOnInteraction: false,
-          // }}
-        >
-          <SwiperSlide className="banner-item">
-              <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/slider-image-1.jpg" />
-          </SwiperSlide>
-          <SwiperSlide className="banner-item">
-              <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/slider-image-2.jpg" />
-          </SwiperSlide>
-          <SwiperSlide className="banner-item">
-              <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/05/slider-3.jpg" />
-          </SwiperSlide>
-        </Swiper>
-      </div>
+    <div className="home-banner">
+      <div className="blank-div"></div>
+      <Swiper
+        slidesPerView={1}
+        // spaceBetween={20}
+        // navigation={true}
+        pagination={true}
+        loop={true}
+        modules={[Navigation, Autoplay, Pagination]}
+        // autoplay={{
+        //   delay: 2500,
+        //   // disableOnInteraction: false,
+        // }}
+      >
+        <SwiperSlide className="banner-item">
+          <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/slider-image-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide className="banner-item">
+          <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/slider-image-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide className="banner-item">
+          <img src="https://klbtheme.com/bacola/wp-content/uploads/2021/05/slider-3.jpg" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 }
