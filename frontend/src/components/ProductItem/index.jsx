471e011c6fa23a3e8dc89df0ebdd066beb56a166
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-01-16 14:39:04 +0700

    Update

--- frontend/src/components/ProductItem/index.jsx
+++ frontend/src/components/ProductItem/index.jsx
@@ -20,47 +20,45 @@ export default function ProductItem({ product }) {
   const currentPrice = getDiscountPrice(product.price, product.discount);
 
   return (
-    <div className="item productItem">
-        <div className="imgWrapper">
-          <Link to={"/product/" + product.slug}>
-            <img
-              src={product.image}
-            />
-          </Link>
-          <div className="actions">
-            <Button onClick={() => viewProductDetails(1)}>
-              <AiOutlineFullscreen />
-            </Button>
-            <Button>
-              <IoMdHeartEmpty />
-            </Button>
+    <div className="product">
+      <div className="product-thumbnail">
+        <Link to={"/product/" + product.slug}>
+          <img src={product.image} />
+        </Link>
+        <div className="actions">
+          <Button onClick={() => viewProductDetails(1)}>
+            <AiOutlineFullscreen />
+          </Button>
+          <Button>
+            <IoMdHeartEmpty />
+          </Button>
+        </div>
+      </div>
+
+      <div className="product-badge">
+        <span className="btn btn--primary">{product.discount}%</span>
+      </div>
+
+      <div className="product-main">
+        <Link to={"/product/" + product.slug}>
+          <h4>{product.name}</h4>
+        </Link>
+        <div className="status">
+          <span className="text-success d-block">In Stock</span>
+          <Rating
+            className="mt-2 mb-2"
+            name="read-only"
+            value={product.rating}
+            readOnly
+            size="small"
+            precision={0.5}
+          />
+          <div className="d-flex">
+            <span className="oldPrice">${product.price}</span>
+            <span className="netPrice text-danger">${currentPrice}</span>
           </div>
         </div>
-  
-        <span className="badge bg-primary">{product.discount}%</span>
-  
-        <div className="product">
-          <Link 
-            to={"/product/" + product.slug}
-            style={{ color: "#333" }}>
-            <h4>{product.name}</h4>
-          </Link>
-          <div className="status">
-            <span className="text-success d-block">In Stock</span>
-            <Rating
-              className="mt-2 mb-2"
-              name="read-only"
-              value={product.rating}
-              readOnly
-              size="small"
-              precision={0.5}
-            />
-            <div className="d-flex">
-              <span className="oldPrice">${product.price}</span>
-              <span className="netPrice text-danger">${currentPrice}</span>
-            </div>
-          </div>
-        </div>
+      </div>
     </div>
   );
 }
