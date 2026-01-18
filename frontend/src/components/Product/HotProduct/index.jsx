61a2e9e8bb4325f265ef878dcad6664e45c31be0
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-01-18 20:55:04 +0700

    Update

--- frontend/src/components/Product/HotProduct/index.jsx
+++ frontend/src/components/Product/HotProduct/index.jsx
@@ -1,9 +1,20 @@
 
-const HotProduct = () => {
+const HotProduct = ({ product }) => {
   return (
     <div className="hot-product">
-        <div className="hot-product-thumbnail"></div>
-        <div className="hot-product-main"></div>
+        <div className="thumbnail">
+            <div className="badge">
+                <span className="btn">{product.discount}%</span>
+            </div>
+            <img src={product.image} alt="NOT FOUND" />
+        </div>
+        <div className="main">
+            <div className="prices"></div>
+            <div className="main">
+                <span className="name"></span>
+                <span className="status"></span>
+            </div>
+        </div>
     </div>
   )
 }
