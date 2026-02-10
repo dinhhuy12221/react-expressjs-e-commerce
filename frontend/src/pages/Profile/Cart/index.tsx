612634aa55f7625502af7539ea75d8280ca70c63
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-02-10 11:12:48 +0700

    Update

--- frontend/src/pages/Profile/Cart/index.tsx
+++ frontend/src/pages/Profile/Cart/index.tsx
@@ -14,6 +14,7 @@ import { useGetProductByIdQuery } from "~/features/product/productApi";
 import { CartInt } from "~/features/cart/cart.types";
 import { getProductById } from "~/api/product";
 import { ProductInt } from "~/features/product/product.types";
+import { getDiscountPrice } from "~/utils/getDiscountPrice";
 
 // const cartProduct = {
 //   img: "https://klbtheme.com/bacola/wp-content/uploads/2021/04/product-image-60-90x90.jpg",
@@ -38,9 +39,11 @@ const Cart = () => {
     const getProductsFromCart = async () => {
       const result = await Promise.all(
         cart.map((item) =>
-          getProductById(item.product_id).then((res) => res.data)
+          getProductById(item.product_id).then((res) => res[0])
         )
       );
+
+      console.log(result);
 
       setProductList(result);
     };
@@ -67,72 +70,82 @@ const Cart = () => {
           </thead>
           <tbody className="profile-page-cart-content-table-body">
             {productList &&
-              productList.map((product: ProductInt, index) => (
-                <tr className="text-center" key={index}>
+              productList.map((item) => (
+                <tr className="profile-page-cart-content-table-body-row">
                   <td width="35%">
-                    <Link to={`product/${product.slug}`}>
-                      <div className="d-flex align-items-center cartItemImgWrapper">
-                        <div className="imgWrapper">
-                          <img src={product.image} className="w-100" />
-                        </div>
-                        <div className="info text-start">
-                          <h6>{product.name}</h6>
-                          <Rating
-                            className="rating"
-                            name="read-only"
-                            defaultValue={product.rating}
-                            precision={0.5}
-                            readOnly
-                          />
-                        </div>
+                    <Link to={`../../product/${item.slug}`}>
+                      <img
+                        className="profile-page-cart-content-table-body-row-image"
+                        src={item.image}
+                      />
+                      <div className="profile-page-cart-content-table-body-row-image">
+                        <h6>{item.name}</h6>
+                        <Rating
+                          className="rating"
+                          name="read-only"
+                          defaultValue={item.rating}
+                          precision={0.5}
+                          readOnly
+                        />
                       </div>
                     </Link>
                   </td>
-                  <td className="unitPrice" width="15%">
-                    <span>{product.price}</span>
-                  </td>
-                  <td className="quantity" width="25%">
-                    <QuantityBox stockQuantity={product.countInStock} />
-                  </td>
-                  <td className="subtotal" width="15%">
-                    <span>{product.countInStock}</span>
-                  </td>
-                  <td className="remove">
+                  <td
+                    className="profile-page-cart-content-table-body-row-price"
+                    width="15%"
+                  >
                     <span>
-                      <IoClose />
+                      {getDiscountPrice(item.price, item.discount).toFixed(2)}
                     </span>
+                  </td>
+                  <td
+                    className="profile-page-cart-content-table-body-row-quantity"
+                    width="25%"
+                  >
+                    <QuantityBox stockQuantity={item.countInStock} />
+                  </td>
+                  <td
+                    className="profile-page-cart-content-table-body-row-total-price"
+                    width="15%"
+                  >
+                    <span>
+                      {(
+                        item.countInStock *
+                        getDiscountPrice(item.price, item.discount)
+                      ).toFixed(2)}
+                    </span>
+                  </td>
+                  <td className="profile-page-cart-content-table-body-row-remove">
+                    <IoClose />
                   </td>
                 </tr>
               ))}
           </tbody>
         </table>
-
-        <div className="col-md-3">
-          <div className="card shadow p-3 cartDetails sticky-top pt-3">
+          <div className="profile-page-cart-content-summary">
             <h4>CART TOTALS</h4>
-            <div className="d-flex align-items-center mb-2">
+            <div className="profile-page-cart-content-summary-subtotal">
               <span>Subtotal</span>
-              <span className="ms-auto text-red font-weight-bold">$3.29</span>
+              <span>$3.29</span>
             </div>
-            <div className="d-flex align-items-center mb-2">
-              <span>Shipping</span>
+            <div className="profile-page-cart-content-summary-delivery">
+              <span>Delivery</span>
               <span className="ms-auto">Free</span>
             </div>
-            <div className="d-flex align-items-center mb-2">
+            <div className="profile-page-cart-content-summary-location">
               <span>Estimated for</span>
-              <span className="ms-auto text-end text-red">United Kingdom</span>
+              <span>United Kingdom</span>
             </div>
-            <div className="d-flex align-items-center">
+            <div className="profile-page-cart-content-summary-total">
               <span>Total</span>
-              <span className="ms-auto text-red">$3.29</span>
+              <span>$3.29</span>
             </div>
 
-            <Button className="bg-red btn--lg btn-round mt-3">
+            <button className="profile-page-cart-content-summary-checkout">
               <IoCartOutline className="me-2" />
               Checkout
-            </Button>
+            </button>
           </div>
-        </div>
       </div>
     </section>
   );
