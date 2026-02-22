6c7abb6cc7c1d1453dc7acda44b78f9b5ced28bd
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-02-22 16:15:44 +0700

    Update

--- frontend/src/pages/Profile/Cart/CartItem/index.tsx
+++ frontend/src/pages/Profile/Cart/CartItem/index.tsx
@@ -1,14 +1,13 @@
-import { Rating } from '@mui/material'
-import React from 'react'
-import { IoClose } from 'react-icons/io5'
-import { Link } from 'react-router-dom'
-import QuantityCounter from '~/components/QuantityCounter'
-import { getDiscountPrice } from '~/utils/getDiscountPrice'
+import { Rating } from "@mui/material";
+import React from "react";
+import { IoClose } from "react-icons/io5";
+import { Link } from "react-router-dom";
+import QuantityCounter from "~/components/QuantityCounter";
+import { getDiscountPrice } from "~/utils/getDiscountPrice";
 
-import "./index.css"
+import "./index.css";
 
 const CartItem = ({ product, selectedIds, setSelectedIds }) => {
-
   const toggleProduct = (id: number) => {
     setSelectedIds((prev) =>
       prev.includes(id) ? prev.filter((x) => x !== id) : [...prev, id]
@@ -17,70 +16,67 @@ const CartItem = ({ product, selectedIds, setSelectedIds }) => {
 
   return (
     <tr className="cart-item">
-          <td>
-            <div className="cart-item-selector">
-              <input
-                type="checkbox"
-                value={product.cartId}
-                checked={selectedIds.includes(product.cartId)}
-                onChange={() => toggleProduct(product.cartId)}
-              />
-            </div>
-          </td>
-          <td>
-            <div className="cart-item-info">
-              <Link to={`../../product/${product.slug}`}>
-                <img
-                  className="cart-item-info-image"
-                  src={product.image}
-                />
-              </Link>
-              <div className="cart-item-info-main">
-                <h4>{product.name}</h4>
-                <Rating
-                  className="rating"
-                  name="read-only"
-                  defaultValue={product.rating}
-                  precision={0.5}
-                  readOnly
-                />
-              </div>
-            </div>
-          </td>
-          <td>
-            <div className="cart-item-price">
-              <span>
-                ${getDiscountPrice(product.price, product.discount).toFixed(2)}
-              </span>
-            </div>
-          </td>
-          <td>
-            <div className="cart-item-quantity">
-              <QuantityCounter
-                value={product.countInCart}
-                onChange={() => {}}
-                stock={product.countInStock}
-              />
-            </div>
-          </td>
-          <td>
-            <div className="cart-item-total-price">
-              <span>
-                $
-                {(
-                  product.countInCart *
-                  getDiscountPrice(product.price, product.discount)
-                ).toFixed(2)}
-              </span>
-            </div>
-          </td>
-          <td>
-            <div className="cart-item-remove">
-              <IoClose className="btn btn--primary btn--rounded" />
-            </div>
-          </td>
-        </tr>
-  )
-}
+      <td>
+        <div className="cart-item-selector">
+          <input
+            type="checkbox"
+            value={product.cartId}
+            checked={selectedIds.includes(product.cartId)}
+            onChange={() => toggleProduct(product.cartId)}
+          />
+        </div>
+      </td>
+      <td>
+        <div className="cart-item-info">
+          <Link to={`../../product/${product.slug}`}>
+            <img className="cart-item-info-image" src={product.image} />
+          </Link>
+          <div className="cart-item-info-main">
+            <h4>{product.name}</h4>
+            <Rating
+              className="rating"
+              name="read-only"
+              defaultValue={product.rating}
+              precision={0.5}
+              readOnly
+            />
+          </div>
+        </div>
+      </td>
+      <td>
+        <div className="cart-item-price">
+          <span>
+            ${getDiscountPrice(product.price, product.discount).toFixed(2)}
+          </span>
+        </div>
+      </td>
+      <td>
+        <div className="cart-item-quantity">
+          <QuantityCounter
+            value={product.countInCart}
+            onChange={() => {}}
+            stock={product.countInStock}
+          />
+        </div>
+      </td>
+      <td>
+        <div className="cart-item-total-price">
+          <span>
+            $
+            {(
+              product.countInCart *
+              getDiscountPrice(product.price, product.discount)
+            ).toFixed(2)}
+          </span>
+        </div>
+      </td>
+      <td>
+        <div className="cart-item-remove">
+          <IoClose className="btn btn--primary btn--rounded" />
+        </div>
+      </td>
+    </tr>
+  );
+};
 
-export default CartItem
+export default CartItem;
