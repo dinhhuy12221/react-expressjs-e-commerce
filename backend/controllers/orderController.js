791e6220b3ce40e04d6e64b5eec813e420149c8b
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-02-25 15:09:56 +0700

    Update

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,45 +1,40 @@
-import Order from '../models/order.js'
+import Order from "../models/order.js";
 
 class orderController {
-    createOrder = async(req, res) => {
-        try {
-            const order = new Order({
-                customerId: req.body.customerId,
-                productId: req.body.productId,
-                productCount: req.body.productCount,
-                // total: req.body.total,
-            })
+  createOrder = async (req, res) => {
+    try {
+      const order = new Order({
+        customerId: req.body.customerId,
+        productId: req.body.productId,
+        productCount: req.body.productCount,
+        // total: req.body.total,
+      });
 
-            const result = await order.save();
+      const result = await order.save();
 
-            if (result) {
-                return res.status(200).json({ message: "Add order successfully" })
-            }
-            return res.status(404).json({
-                message: 'failed',
-            })
-        } catch (error) {
-            console.log(error);
-        }
+      return res.status(200).json({ message: "Add order successfully", data: result });
+    } catch (error) {
+      console.log(error);
+      return res.status(404).json({
+        message: "Internal server error",
+      });
     }
+  };
 
-    getOrderByCustomer = async(req, res) => {
-        try {
-            const order = await Order.find({
-                customerId: req.params.id,
-            })
+  getOrderByCustomer = async (req, res) => {
+    try {
+      const order = await Order.find({
+        customerId: req.params.id,
+      });
 
-            if (order) {
-                return res.status(200).json(order)
-            }
-            return res.status(400).json({
-                message: 'failed'
-            })
-        } catch (error) {
-            console.log(error);
-        }
+      return res.status(200).json(order);
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({
+        message: "Internal server error",
+      });
     }
+  };
 }
-
 
 export default new orderController();
