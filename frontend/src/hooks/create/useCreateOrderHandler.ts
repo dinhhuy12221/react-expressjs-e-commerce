477d831391056ca07db5d6310c45486ab6f55119
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-02-26 23:56:49 +0700

    Update

--- frontend/src/hooks/create/useCreateOrderHandler.ts
+++ frontend/src/hooks/create/useCreateOrderHandler.ts
@@ -1,10 +1,24 @@
 import { useCreateOrderMutation } from "~/features/orders/ordersApi"
+
+type product = {
+  id: number,
+  count: number,
+}
+
 const useCreateOrderHandler = () => {
   const [createOrder, mutationState] = useCreateOrderMutation();
 
-  const handleCreateOrder = () => {
-    
+  const handleCreateOrder = async (customerId: number, products: Array<product>, location: string, delivery: number) => {
+    const payload = {
+      customerId,
+      products,
+      location,
+      delivery,
+    }
+    await createOrder(payload).unwrap();
   }
+
+  return { handleCreateOrder, ...mutationState };
 }
 
 export default useCreateOrderHandler
