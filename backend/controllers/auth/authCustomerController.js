007f8a11aac8a2c5358d043d16f96f6f36cd3f47
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-03-02 22:33:57 +0700

    Update

--- backend/controllers/auth/authCustomerController.js
+++ backend/controllers/auth/authCustomerController.js
@@ -1,17 +1,17 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import CustomerAccount from "../../models/account/customerAccount.js";
-import Customer from '../../models/customer.js'
+import Customer from "../../models/customer.js";
 
-const REFRESH_TOKEN_EXPIRATION = '1h'
-const ACCESS_TOKEN_EXPIRATION = '30m'
+const REFRESH_TOKEN_EXPIRATION = "1h";
+const ACCESS_TOKEN_EXPIRATION = "30m";
 
 class authCustomerController {
   login = async (req, res) => {
     try {
       const { username, password } = req.body;
       // console.log(username, password);
-      
+
       if (!username || !password) {
         return res.status(400).json({ message: "All fields are required" });
       }
@@ -21,21 +21,27 @@ class authCustomerController {
       });
 
       if (!account) {
-        return res.status(401).json({ message: "Unauthorized (Account not found)" });
+        return res
+          .status(401)
+          .json({ message: "Unauthorized (Account not found)" });
       }
-      
+
       const match = await bcrypt.compare(password, account.password);
-      
+
       if (!match) {
-        return res.status(401).json({ message: "Unauthorized (Password is incorrect)" });
+        return res
+          .status(401)
+          .json({ message: "Unauthorized (Password is incorrect)" });
       }
-      
+
       const customer = await Customer.findOne({
         username: username,
-      })
-      
+      });
+
       if (!customer) {
-        return res.status(401).json({ message: "Unauthorized (Customer not found)" });
+        return res
+          .status(401)
+          .json({ message: "Unauthorized (Customer not found)" });
       }
 
       const accessToken = jwt.sign(
@@ -69,7 +75,7 @@ class authCustomerController {
         maxAge: 60 * 60 * 1000,
       });
 
-      res.status(200).json({ "accessToken": accessToken, "customer": customer });
+      res.status(200).json({ accessToken: accessToken, customer: customer });
     } catch (error) {
       console.log(error);
     }
@@ -79,7 +85,9 @@ class authCustomerController {
     const cookies = req.cookies;
 
     if (!cookies?.jwt) {
-      return res.status(401).json({ message: "Unauthorized (jwt not existed)" });
+      return res
+        .status(401)
+        .json({ message: "Unauthorized (jwt not existed)" });
     }
 
     const refreshToken = cookies.jwt;
@@ -94,7 +102,10 @@ class authCustomerController {
           username: decoded.username,
         }).exec();
 
-        if (!account) return res.status(401).json({ message: "Unauthorized (account is not existed)" });
+        if (!account)
+          return res
+            .status(401)
+            .json({ message: "Unauthorized (account is not existed)" });
 
         const accessToken = jwt.sign(
           {
@@ -112,22 +123,33 @@ class authCustomerController {
   };
 
   logout = async (req, res) => {
-    const cookies = req.cookies;
-    if (!cookies?.jwt) return res.sendStatus(204);
+    try {
+      const cookies = req.cookies;
+      if (!cookies?.jwt) return res.sendStatus(204);
 
-    const refreshToken = cookies.jwt;
-    const account = await CustomerAccount.findOneAndUpdate({ refreshToken: refreshToken }, { refreshToken: '' });
-    if (!account) {
-      res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true });
-      return res.sendStatus(204);
+      const refreshToken = cookies.jwt;
+      const account = await CustomerAccount.findOneAndUpdate(
+        { refreshToken: refreshToken },
+        { refreshToken: "" }
+      );
+      if (!account) {
+        res.clearCookie("jwt", {
+          httpOnly: true,
+          sameSite: "None",
+          secure: true,
+        });
+        return res.sendStatus(204);
+      }
+
+      res.clearCookie("jwt", {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+      });
+      res.json({ message: "Cookie cleared" });
+    } catch (error) {
+      console.log(error);
     }
-    
-    res.clearCookie("jwt", {
-      httpOnly: true,
-      sameSite: "none",
-      secure: true,
-    });
-    res.json({ message: "Cookie cleared" });
   };
 }
 
