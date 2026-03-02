9ed0b83e09c2650dcc5182b8f1835378e5760074
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-03-02 13:33:50 +0700

    Update

--- frontend/src/pages/Profile/Order/OrderItem/index.tsx
+++ frontend/src/pages/Profile/Order/OrderItem/index.tsx
@@ -1,20 +1,25 @@
 import React from "react";
 
 import "./index.css";
+import { getDiscountPrice } from "~/utils/getDiscountPrice";
 
 const OrderItem = ({ order }) => {
   return (
     <div className="order-item">
       <h3 className="order-item-id">#{order._id}</h3>
       <div className="order-item-content">
-          <div>
-            Products:{" "}
             {order.products.map((i) => (
-              <span>
-                {i.count}, {i.price}, {i.discount}, {i.id.name}
-              </span>
+              <div className="order-item-product">
+                <img className="order-item-product-thumbnail" src={i.id.image} />
+                <div className="order-item-product-content">
+                  <h2>{i.id.name}</h2>
+                  <div className="order-item-product-content-prices">
+                    <span className="order-item-product-content-prices-old-price">{i.price}</span>
+                    <span className="order-item-product-content-prices-net-price">{getDiscountPrice(i.price, i.discount).toFixed(2)}</span>
+                  </div>
+                </div>
+              </div>
             ))}
-          </div>
           <div>Delivery: {order.delivery}</div>
           <div>Location: {order.location}</div>
       </div>
