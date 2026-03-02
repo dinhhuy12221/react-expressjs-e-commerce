ba5cd4eb6abf2a18d4322c589328bcb22fb7ee86
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-03-02 22:25:16 +0700

    Update

--- frontend/src/components/Header/Profile/index.tsx
+++ frontend/src/components/Header/Profile/index.tsx
@@ -1,37 +1,49 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 
-import { useDispatch } from "react-redux";
-import { logOut } from "~/features/auth/authSlice";
-import { useLogoutMutation } from '~/features/auth/authApi';
+import { useDispatch, useSelector } from "react-redux";
+import { logOut, selectCurrentCustomerId } from "~/features/auth/authSlice";
+import { useLogoutMutation } from "~/features/auth/authApi";
 import { Link, useNavigate } from "react-router-dom";
 import { FaRegUserCircle } from "react-icons/fa";
-import './index.scss'
+import { MyContext } from "~/App";
+
+import "./index.css";
 
 function Profile() {
+  const customerId = useSelector(selectCurrentCustomerId);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [isActive, setIsActive] = useState(false);
+  const { setIsLoading } = useContext(MyContext);
 
-  const [ logout, { isLoading } ] = useLogoutMutation();
+  const [logout, { isLoading }] = useLogoutMutation();
 
-    const handleLogOut = async () => {
-      try {
-        await logout(null).unwrap()
-      } catch (error) {
-
-      } finally {
-        dispatch(logOut())
-        navigate(0)
-      }
+  const handleLogOut = async () => {
+    try {
+      await logout(null).unwrap();
+    } catch (error) {
+    } finally {
+      dispatch(logOut());
+      navigate(0);
     }
+  };
+
+  useEffect(() => {
+    setIsLoading(isLoading);
+  }, [isLoading]);
 
   return (
     <div className="header-profile">
-      <Link className='profile-link' to="/profile/information">
+      <button className="header-profile-button" onClick={() => setIsActive(true)}>
         <FaRegUserCircle />
-      </Link>
-      <button className="btn btn--outlined" onClick={handleLogOut}>
-        Logout
       </button>
+      {isActive && <div className="header-profile-dropbox">
+        {
+          customerId ? <button className="header-profile-dropbox-logout" onClick={handleLogOut}>
+          Logout
+        </button> : <button>Signin</button>
+        }
+      </div>}
     </div>
   );
 }
