589330fbe34f68efc97bbae5ab26353f078fb4f2
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-03-04 16:55:21 +0700

    Update

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -24,12 +24,12 @@
 
 // export default App;
 
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>
+  // </React.StrictMode>
 );
