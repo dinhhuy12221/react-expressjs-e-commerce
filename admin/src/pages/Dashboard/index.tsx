67550c01e9251b6658d7a4a7104011ee37831589
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-03-09 11:19:17 +0700

    Update

--- admin/src/pages/Dashboard/index.tsx
+++ admin/src/pages/Dashboard/index.tsx
@@ -65,38 +65,32 @@ const Dashboard = () => {
   const products = new Array(10).fill(
     <tr>
       <td>#1</td>
-      <td>
-        <div className="d-flex align-items-center productBox">
-          <div className="imgWrapper">
-            <div className="img">
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWcJNHzYZB8D673M2BdfeunxmGpEw01441aQ&s"
-                className="w-100"
-              />
-            </div>
-          </div>
-          <div className="info ps-0">
-            <h6>Top and skirt set for Female</h6>
-            <p>
+      <td className="dashboard-table-content-main-image">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWcJNHzYZB8D673M2BdfeunxmGpEw01441aQ&s"
+          />
+      </td>
+      <td className="dashboard-table-content-main-name">
+            <span className="dashboard-table-content-main-name-head">Top and skirt set for Female</span>
+            <br />
+            <span>
               Women's exclusive summer Tops and skirt set for Female Tops and
               skirt set
-            </p>
-          </div>
-        </div>
+            </span>
       </td>
-      <td>womans</td>
-      <td>richman</td>
+      <td className="dashboard-table-content-main-category">Women</td>
+      <td className="dashboard-table-content-main-brand">Rich man</td>
       <td>
-        <div style={{ width: "70px" }}>
+        <div className="dashboard-table-content-main-prices" style={{ width: "70px" }}>
           <del className="old">$21.00</del>
           <span className="new text-danger">$19.00</span>
         </div>
       </td>
-      <td>30</td>
-      <td>4.9(16)</td>
-      <td>380</td>
-      <td>$38k</td>
-      <td>
+      <td className="dashboard-table-content-main-stock">30</td>
+      <td className="dashboard-table-content-main-rating">4.9(16)</td>
+      <td className="dashboard-table-content-main-order">380</td>
+      <td className="dashboard-table-content-main-sales">$38k</td>
+      <td className="dashboard-table-content-main-actions">
         <div className="actions d-flex align-items-center justify-content-center">
           <Button className="secondary" color="secondary">
             <FaEye />
@@ -129,163 +123,162 @@ const Dashboard = () => {
   };
 
   return (
-    <>
-      <div className="dashboard">
-        <div className="dashboard-content">
-          <div className="dashboard-content-overview">
-            <DashboardBox
-              className="dashboard-content-overview-item"
-              color={["#1da256", "#48d483"]}
-              icon={<FaUserCircle />}
-              grow={true}
-            />
-            <DashboardBox
-              className="dashboard-content-overview-item"
-              color={["#c012e2", "#eb64fe"]}
-              icon={<RiShoppingCart2Fill />}
-              grow={false}
-            />
-            <DashboardBox
-              className="dashboard-content-overview-item"
-              color={["#2c78e5", "#60aff5"]}
-              icon={<IoBagHandleSharp />}
-              grow={true}
-            />
-            <DashboardBox
-              className="dashboard-content-overview-item"
-              color={["#e1950e", "#f3cd29"]}
-              icon={<MdOutlineStar />}
-              grow={false}
-            />
-          </div>
+    <div className="dashboard">
+      <div className="dashboard-content">
+        <div className="dashboard-content-overview">
+          <DashboardBox
+            className="dashboard-content-overview-item"
+            color={["#1da256", "#48d483"]}
+            icon={<FaUserCircle />}
+            grow={true}
+          />
+          <DashboardBox
+            className="dashboard-content-overview-item"
+            color={["#c012e2", "#eb64fe"]}
+            icon={<RiShoppingCart2Fill />}
+            grow={false}
+          />
+          <DashboardBox
+            className="dashboard-content-overview-item"
+            color={["#2c78e5", "#60aff5"]}
+            icon={<IoBagHandleSharp />}
+            grow={true}
+          />
+          <DashboardBox
+            className="dashboard-content-overview-item"
+            color={["#e1950e", "#f3cd29"]}
+            icon={<MdOutlineStar />}
+            grow={false}
+          />
+        </div>
 
-          <div className="dashboard-content-graph">
-            <div className="dashboard-content-graph-header">
-              <h3 className="dashboard-content-graph-header-title">
-                Total Sales
-              </h3>
-              <div className="ms-auto dashboard-content-graph-header-expand">
-                <Button className="" onClick={handleClick}>
-                  <HiOutlineDotsVertical />
-                </Button>
-                <Menu
-                  id="long-menu"
-                  MenuListProps={{
-                    "aria-labelledby": "long-button",
-                  }}
-                  anchorEl={anchorEl}
-                  open={open}
-                  onClose={handleClose}
-                  slotProps={{
-                    paper: {
-                      style: {
-                        maxHeight: ITEM_HEIGHT * 4.5,
-                      },
-                    },
-                  }}
-                >
-                  {options1.map((option) => (
-                    <MenuItem
-                      key={option}
-                      //   selected={option === "Pyxis"}
-                      onClick={handleClose}
-                    >
-                      <IoTimerOutline className="me-2" />
-                      {option}
-                    </MenuItem>
-                  ))}
-                </Menu>
-              </div>
-            </div>
-            <h3 className="dashboard-content-graph-header-total">
-              $3,333,333.33
+        <div className="dashboard-content-graph">
+          <div className="dashboard-content-graph-header">
+            <h3 className="dashboard-content-graph-header-title">
+              Total Sales
             </h3>
-            <p>Last Month: $3,123.45</p>
-            <Chart
-              className="dashboard-content-graph-main"
-              chartType="AreaChart"
-              width={"100%"}
-              height={"100%"}
-              data={data}
-              options={options2}
-            />
+            <div className="ms-auto dashboard-content-graph-header-expand">
+              <Button className="" onClick={handleClick}>
+                <HiOutlineDotsVertical />
+              </Button>
+              <Menu
+                id="long-menu"
+                MenuListProps={{
+                  "aria-labelledby": "long-button",
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                slotProps={{
+                  paper: {
+                    style: {
+                      maxHeight: ITEM_HEIGHT * 4.5,
+                    },
+                  },
+                }}
+              >
+                {options1.map((option) => (
+                  <MenuItem
+                    key={option}
+                    //   selected={option === "Pyxis"}
+                    onClick={handleClose}
+                  >
+                    <IoTimerOutline className="me-2" />
+                    {option}
+                  </MenuItem>
+                ))}
+              </Menu>
+            </div>
+          </div>
+          <h3 className="dashboard-content-graph-header-total">
+            $3,333,333.33
+          </h3>
+          <p>Last Month: $3,123.45</p>
+          <Chart
+            className="dashboard-content-graph-main"
+            chartType="AreaChart"
+            width={"100%"}
+            height={"100%"}
+            data={data}
+            options={options2}
+          />
+        </div>
+      </div>
+
+      <div className="dashboard-table">
+        <h3>Best Selling Products</h3>
+
+        <div className="dashboard-table-filter">
+          <div className="dashboard-table-filter-number">
+            <h4>Show by</h4>
+            <FormControl size="small">
+              <Select
+                value={showBy}
+                onChange={(e) => setShowBy(e.target.value)}
+                displayEmpty
+                inputProps={{ "aria-label": "Without label" }}
+                labelId="demo-simple-select-helper-label"
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className="dashboard-table-filter-category">
+            <h4>Categorie by</h4>
+            <FormControl size="small">
+              <Select
+                value={showByCat}
+                onChange={(e) => setShowByCat(e.target.value)}
+                displayEmpty
+                inputProps={{ "aria-label": "Without label" }}
+                labelId="demo-simple-select-helper-label"
+                className="w-100"
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
           </div>
         </div>
 
-        <div className="dashboard-table">
-          <h3>Best Selling Products</h3>
-
-          <div className="dashboard-table-filter">
-            <div className="dashboard-table-filter-number">
-              <h4>Show by</h4>
-              <FormControl size="small">
-                <Select
-                  value={showBy}
-                  onChange={(e) => setShowBy(e.target.value)}
-                  displayEmpty
-                  inputProps={{ "aria-label": "Without label" }}
-                  labelId="demo-simple-select-helper-label"
-                >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
-            </div>
-            <div className="dashboard-table-filter-category">
-              <h4>Categorie by</h4>
-              <FormControl size="small">
-                <Select
-                  value={showByCat}
-                  onChange={(e) => setShowByCat(e.target.value)}
-                  displayEmpty
-                  inputProps={{ "aria-label": "Without label" }}
-                  labelId="demo-simple-select-helper-label"
-                  className="w-100"
-                >
-                  <MenuItem value="">
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
-                </Select>
-              </FormControl>
-            </div>
-          </div>
-
-          <div className="table-responsive mt-3">
-            <table className="table table-bordered table-hover v-align">
-              <thead className="table-dark">
-                <tr>
-                  <th>UID</th>
-                  <th style={{ width: "300px" }}>PRODUCT</th>
-                  <th>CATEGORY</th>
-                  <th>BRAND</th>
-                  <th>PRICE</th>
-                  <th>STOCK</th>
-                  <th>RATING</th>
-                  <th>ORDER</th>
-                  <th>SALES</th>
-                  <th>ACTION</th>
-                </tr>
-              </thead>
-              <tbody>
-                {products.map(item => item)}
-              </tbody>
-            </table>
-            <div className="d-flex tableFooter">
-              <p>
-                Showing <b>12</b> of <b>60</b> results
-              </p>
+        <div className="dashboard-table-content">
+          <table className="table table-bordered table-hover v-align dashboard-table-content-main">
+            <thead className="table-dark">
+              <tr>
+                <th>UID</th>
+                <th style={{ width: "250px" }}>IMAGE</th>
+                <th>NAME</th>
+                <th>CATEGORY</th>
+                <th>BRAND</th>
+                <th>PRICE</th>
+                <th>STOCK</th>
+                <th>RATING</th>
+                <th>ORDER</th>
+                <th>SALES</th>
+                <th>ACTION</th>
+              </tr>
+            </thead>
+            <tbody>{products.map((item) => item)}</tbody>
+          </table>
+          <div className="dashboard-table-content-footer">
+            <p>
+              Showing <b>12</b> of <b>60</b> results
+            </p>
+            <div className="dashboard-table-content-footer-pagination"
+>
               <Pagination
                 count={10}
                 color="primary"
                 variant="outlined"
-                className="pagination"
                 showFirstButton
                 showLastButton
               />
@@ -293,7 +286,7 @@ const Dashboard = () => {
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
