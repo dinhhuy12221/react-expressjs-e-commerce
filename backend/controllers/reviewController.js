78cd324aa74b2ea0c361bf6b577f9c4026efe2a7
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-03-12 22:20:58 +0700

    Update

--- backend/controllers/reviewController.js
+++ backend/controllers/reviewController.js
@@ -1,5 +1,30 @@
 import Review from "../models/review.js";
 
 class reviewController {
-    
+    createReview = async (req, res) => {
+        try {
+            const { customerId, productId, content, rating } = req.body;
+            const payload = {
+                customerId,
+                productId,
+                content,
+                rating
+            }
+            const review = await Review.create(payload);
+
+            res.status(201).json({ message: "Review created successfully", data: review })
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: "Internal server error", error: error.message })
+        }
+    }
+
+    getReviewByProductId = async (req, res) => {
+        try {
+            
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: "Internal server error", error: error.message })
+        }
+    }
 }
