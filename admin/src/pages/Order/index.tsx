b38172a1b06abc1a49e8e3f215ffc6027eda17b3
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-03-14 21:23:12 +0700

    Update

--- admin/src/pages/Order/index.tsx
+++ admin/src/pages/Order/index.tsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { getOrders } from "../../api/order";
+
+import "./index.css";
+import Breadcrumb from "../../components/Breadcrumb";
 
 const Order = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [orders, setOrders] = useState<any>(null);
 
-export default Order
+  useEffect(() => {
+    const handleAsync = async () => {
+      const result = await getOrders();
+      setOrders(result);
+    };
+
+    handleAsync();
+  }, []);
+  return (
+    <div className="order">
+      <Breadcrumb
+        path={[
+          {
+            name: "Orders",
+            to: `/orders`,
+          },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default Order;
