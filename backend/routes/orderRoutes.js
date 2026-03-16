bb4e3b096610f89ce8ff6162c0863675a9118570
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-03-16 22:39:23 +0700

    Update

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.get('/orders_by_customer/:id', orderController.getOrderByCustomer)
 router.post('/create', orderController.createOrder);
+router.get('/:id', orderController.getOrderById);
 router.get('/', orderController.getOrders);
 
 export default router;
