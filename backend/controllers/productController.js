1f65cd7fb97ad1b85ec849026c753b38dfd86c48
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-03-19 16:15:16 +0700

    Update

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -158,8 +158,16 @@ class productController {
             console.log(parsedImages[index].public_id);
 
             if (parsedImages[index].public_id) {
-              console.log("yes");
-
+              const result = await cloudinary.v2.uploader.upload(file.path, {
+                public_id: parsedImages[index]?.public_id,
+                overwrite: true,
+              });
+              
+              return {
+                url: result.secure_url || "",
+                public_id: result.public_id || "",
+              };
+            } else {
               const result = await cloudinary.v2.uploader.upload(file.path, {
                 folder: `ecommerce/products/${req.body._id}`,
               });
@@ -167,16 +175,6 @@ class productController {
               return {
                 url: result.secure_url,
                 public_id: result.public_id,
-              };
-            } else {
-              const result = await cloudinary.v2.uploader.upload(file.path, {
-                public_id: parsedImages[index]?.public_id,
-                overwrite: true,
-              });
-
-              return {
-                url: result.secure_url || "",
-                public_id: result.public_id || "",
               };
             }
           } else {
