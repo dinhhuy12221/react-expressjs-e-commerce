fa0bede451b091c73020c7bc110ab3170f13696d
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-03-23 22:25:33 +0700

    Update

--- backend/middlewares/verifyJWT.js
+++ backend/middlewares/verifyJWT.js
@@ -9,13 +9,14 @@ const verifyJWT = (req, res, next) => {
 
 
     jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) return res.sendStatus(403);
+      if (err) return res.status(403);
       req.username = decoded.username;
       // next();
       return res.status(200).json({ message: "Success", username: req.username || "" });
     });
   } catch (error) {
     console.log(error);
+    res.status(500).json({ message: "Internal server error", error: error.message })
   }
 };
 
--- backend/routes/account/userAccountRoutes.js
+++ backend/routes/account/userAccountRoutes.js
@@ -3,7 +3,7 @@ import userController from '../../controllers/userController.js';
 import express from 'express'
 const router = express.Router();
 
-router.post('/create', userAccountController.create, userController.createCustomer);
+router.post('/create', userAccountController.create, userController.createUser);
 router.post('/get', userAccountController.get);
 
 export default router
