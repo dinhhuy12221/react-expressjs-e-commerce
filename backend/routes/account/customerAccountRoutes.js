2262b977c8ff6505ffeb26c95e989843a71c9392
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-03-24 15:56:35 +0700

    Update

--- backend/routes/account/customerAccountRoutes.js
+++ backend/routes/account/customerAccountRoutes.js
@@ -1,9 +1,9 @@
-import userAccountController from '../../controllers/account/userAccountController.js';
-import userController from '../../controllers/userController.js';
+import customerAccountController from '../../controllers/account/customerAccountController.js';
+import customerController from '../../controllers/customerController.js';
 import express from 'express'
 const router = express.Router();
 
-router.post('/create', userAccountController.create, userController.createUser);
-router.post('/get', userAccountController.get);
+router.post('/create', customerAccountController.create, customerController.createCustomer);
+router.post('/get', customerAccountController.get);
 
 export default router
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,16 +8,21 @@ import authCustomerRoutes from "./auth/authCustomerRoutes.js";
 import customerAccountRoutes from "./account/customerAccountRoutes.js";
 import customerRoutes from "./customerRoutes.js";
 import userAccountRoutes from "./account/customerAccountRoutes.js";
+import authUserRoutes from "./auth/authUserRoutes.js";
 import userRoutes from "./userRoutes.js";
 import verifyRoutes from "./verifyRoutes.js";
 
 const route = (app) => {
   app.use("/api/auth/verify", verifyRoutes);
+
   app.use("/api/auth/customer", authCustomerRoutes);
   app.use("/api/account/customer", customerAccountRoutes);
-  app.use("/api/auth/user", userAccountRoutes);
-  app.use("/api/account/user", userRoutes);
   app.use("/api/customer", customerRoutes);
+
+  app.use("/api/auth/user", authUserRoutes);
+  app.use("/api/account/user", userAccountRoutes);
+  app.use("/api/user", userRoutes);
+  
   app.use("/api/category", categoryRouter);
   app.use("/api/brand", brandRouter);
   app.use("/api/product", productRouter);
