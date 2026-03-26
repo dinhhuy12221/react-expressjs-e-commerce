14687aa7da97d946f57b253898b5bbd7451f66bf
LAPTOP-QNC29JHR\HuyDinh <dinhhuy574@gmail.com>
2026-03-26 22:15:53 +0700

    Update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -93,15 +93,27 @@ class userController {
             public_id: result.public_id,
           };
         }
-      } else if (parsedImage.public_id) {
-        const result = await cloudinary.v2.uploader.upload(filePath, {
-          folder: `ecommerce/users/${req.body._id}`,
-        });
+      } else {
+        if (parsedImage.public_id) {
+          const result = await cloudinary.v2.uploader.upload(filePath, {
+            public_id: parsedImage.public_id,
+            overwrite: true,
+          });
 
-        image_result = {
-          url: result.secure_url,
-          public_id: result.public_id,
-        };
+          image_result = {
+            url: result.secure_url,
+            public_id: result.public_id,
+          };
+        } else {
+          const result = await cloudinary.v2.uploader.upload(filePath, {
+            folder: `ecommerce/users/${req.body._id}`,
+          });
+
+          image_result = {
+            url: result.secure_url,
+            public_id: result.public_id,
+          };
+        }
       }
 
       await fs.unlink(file?.path).catch(() => {});
